30bd4c45076e8a3ab4f6d4d35e8b9ac6b3f4eda6
Assumpta-tech <uwamariya.assumpta@yahoo.com>
2024-11-14 22:49:48 +0200

    changed the layout and worked on the signup pages

---
+++ WebProject.js
@@ -0,0 +1,8 @@
+let signUp = prompt("Enter what defines you, BUYER or SELLER");
+signUp = signUp.toLowerCase();
+
+let button = document.querySelector(".navigation");
+if (signUp === buyer) {
+  signUpBuyer(url(MYWEBPOTIFORIO / SignUp.html));
+}
+function signUpBuyer() {}
